788c420b398c97815eb12cc0584d8744cfc299ef
Olga Naboichenko <olga140404ol@gmail.com>
2022-05-27 16:48:03 +0300

    home task 4

--- index.js
+++ index.js
@@ -2,24 +2,18 @@ const firstNumber = Number(prompt('Enter first number'))
 const operation = prompt('Enter operator')
 const secondNumber = Number(prompt('Enter second number'))
 
-if (firstNumber !== firstNumber) {  console.log('Is NaN');
-} else {
-  console.log('Is Not NaN');
-}
+if((isNaN(firstNumber)) || (isNaN(secondNumber))){
+    alert('некорректный ввод')
+} else
 
-if (secondNumber !== secondNumber) {  console.log('Is NaN');
-} else {
-  console.log('Is Not NaN');
-}
-
-if (operation === '-'){
-    alert('Ваш результат ' + (firstNumber - secondNumber));
-} else if (operation === '+'){
-    alert('Ваш результат ' + (firstNumber + secondNumber));
-} else if (operation === '*'){
-    alert('Ваш результат ' + (firstNumber * secondNumber));
-} else if (operation === '/'){
-    alert('Ваш результат ' + (firstNumber / secondNumber));
-} else {
-    alert('Такой операции не существует')
-}
+    if (operation === '-'){
+        alert('Ваш результат ' + (firstNumber - secondNumber));
+    } else if (operation === '+'){
+        alert('Ваш результат ' + (firstNumber + secondNumber));
+    } else if (operation === '*'){
+        alert('Ваш результат ' + (firstNumber * secondNumber));
+    } else if (operation === '/'){
+        alert('Ваш результат ' + (firstNumber / secondNumber));
+    } else {
+        alert('Такой операции не существует')
+    }
